348140650041f3617efc099e3a629c311917a264
ankit <coolankit37411@gmail.com>
2022-06-02 00:03:54 +0530

    login backend

--- src/main/java/com/backend/controller/ControllerClass.java
+++ src/main/java/com/backend/controller/ControllerClass.java
@@ -2,6 +2,7 @@ package com.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,11 @@ public class ControllerClass {
 	@Autowired
 	private InstaRepository instaRepository;
 	
+	
+	@GetMapping("/hello")
+	public String onetwoth() {
+		return "hello world";
+	}
 	
 @PostMapping("/request")
 public String save(@RequestBody Login login) {
